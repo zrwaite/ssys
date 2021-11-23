fd66d33cd3ba27e8dbd5ce574d5c1fb12ce87e47
Zac Waite <129032699zw@gmail.com>
2021-11-23 16:17:32 -0500

    Testing file

--- backend/modules/mysqltest.php
+++ backend/modules/mysqltest.php
@@ -1,12 +1,15 @@
 <?php
-require './database.php';
-require '../models/response.php';
-$res = new Response;
-// Check connection
-if($conn->connect_error) {
-	array_push($res->errors, $conn->connect_error);
-	echo $res->errors;
-} 
-echo "Connected successfully";
-$conn->close();
+if (file_exists(__DIR__."/database.php")){
+	//IMPORTS
+	require __DIR__.'/database.php';
+
+	//MAIN
+	$accounts = DB::query("SELECT * FROM ssys22_students");
+	foreach ($accounts as $account) {
+		echo $account['id'] . "\n";
+	}
+	echo "Connected successfully";
+
+} else {echo "Import error";}
+//https://stackoverflow.com/questions/5371828/relative-path-in-require-once-doesnt-work
 ?>
