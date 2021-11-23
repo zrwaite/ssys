17d8e91c6a3163108d0553d69de2d9924e10a79c
Zac Waite <129032699zw@gmail.com>
2021-11-23 16:21:06 -0500

    First endpoint and help functions

--- backend/api/student/postStudent.php
+++ backend/api/student/postStudent.php
@@ -1,20 +1,66 @@
 <?php
-class PostStudent { //Class for json response 
-    public $email;
-    public $fname;
-    public $lname;
-    public $password;
-    public $hash;
-    public function __construct(){
-    }
-	public function createPassword(){
-		$chars = 10;
-		$data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
-  		$this->password =  substr(str_shuffle($data), 0, $chars);
+
+use Symfony\Component\Dotenv\Dotenv;
+
+
+if (file_exists(__DIR__."/../../vendor/autoload.php")
+&& file_exists(__DIR__."/../../modules/database.php")
+&& file_exists(__DIR__."/../../modules/env/.env")
+&& file_exists(__DIR__."/../../models/response.php")
+&& file_exists(__DIR__."/../../models/student/postStudent.php")){
+	//Imports
+	require_once __DIR__."/../../vendor/autoload.php";
+	require_once __DIR__."/../../models/response.php";
+	require_once __DIR__."/../../modules/database.php"; //Connect to database
+	require_once __DIR__."/../../models/student/postStudent.php";
+
+	$dotenv = new Dotenv();
+	$dotenv->load(__DIR__."/../../modules/env/.env");
+
+	//Main
+	#Object declaraion
+	$res = new Response();
+	$student = new PostStudent();
+
+	#get post queries
+	$email = getPost("email");
+	if($email) $student->email = $email;
+	else array_push($res->errors, "Must include email");
+
+	$fname = getPost("fname");
+	if ($fname) $student->fname = $fname;
+	else $student->fname = "Person";
+
+	$lname = getPost("lname");
+	if ($lname) $student->lname = $lname;
+	else $student->lname = "Smith";
+
+	$password = getPost("password");
+	if ($password) $student->password = $password;
+	else $student->createPassword();
+
+	$teacher_email = getPost("teacher_email");
+	if ($teacher_email) $student->teacher_email = $teacher_email;
+	else $student->teacher_email = null;
+
+	$teacher_id = getPost("teacher_id");
+	if($teacher_id) $student->teacher_id = $teacher_id;
+	else $student->teacher_id = null;
+
+	if (count($res->errors)==0){
+		$student->createHash();
+		// DB::insert('ssys22_students', array(
+		// 	'email' => $student->email,
+		// 	'fname' => $student->fname,
+		// 	'lname' => $student->lname,
+		// 	'password_hash' => $student->hash,
+		// 	'teacher_email' => $student->teacher_email,
+		// 	'teacher_id' => $student->teacher_id
+		// ));
+		// $res->status = 200;
+		// $res->success = true;
 	}
-	public function createHash(){
-		$this->hash = password_hash($this->password, PASSWORD_DEFAULT);
-	}
-}
-echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+	echo json_encode($student);
+	echo json_encode($res);
+} else {echo "Import error";}
 ?>
