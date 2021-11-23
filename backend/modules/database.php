8e2a5d0dc8fa3b64af2590eee531941e884026b9
Zac Waite <129032699zw@gmail.com>
2021-11-23 16:17:45 -0500

    Database connection finalized

--- backend/modules/database.php
+++ backend/modules/database.php
@@ -1,8 +1,21 @@
 <?php
-//Allows for .env variables to be loaded in
-require '../vendor/autoload.php';
-$dotenv = new Symfony\Component\Dotenv\Dotenv();
-$dotenv->load(__DIR__.'/env/.env');
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = mysqli_connect($_ENV['SERVER'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DBNAME']); //Connect to database
+use Symfony\Component\Dotenv\Dotenv;
+if (file_exists(__DIR__."/../vendor/autoload.php")
+&& file_exists(__DIR__.'/meekrodb-2.4/db.class.php')
+&& file_exists(__DIR__.'/env/.env')){
+	//Imports
+	require_once __DIR__."/../vendor/autoload.php";
+	require_once __DIR__.'/meekrodb-2.4/db.class.php';
+	$dotenv = new Dotenv();
+	$dotenv->load(__DIR__.'/env/.env');
+
+	//Main
+
+	DB::$user = $_ENV['USERNAME'];
+	DB::$password = $_ENV['PASSWORD'];
+	DB::$dbName = $_ENV['DBNAME'];
+
+
+} else {echo "Import error";}
+//https://stackoverflow.com/questions/5371828/relative-path-in-require-once-doesnt-work
 ?>
