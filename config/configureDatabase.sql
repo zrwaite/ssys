0c799aea1687160d029faaf8c79a9cb7499f287c
Zac Waite <129032699zw@gmail.com>
2021-11-23 16:21:17 -0500

    Database config file

--- config/configureDatabase.sql
+++ config/configureDatabase.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE sustaina_db;
 
-CREATE TABLE sustaina_db.ssyc22_teachers (
+CREATE TABLE sustaina_db.ssys22_teachers (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	first_name CHAR(20) NOT NULL,
-    last_name CHAR(20) NOT NULL,
+	fname CHAR(20) NOT NULL,
+    lname CHAR(20) NOT NULL,
     email CHAR(40) NOT NULL,
 	email_confirmed BOOLEAN DEFAULT 0,
 	confirmation_code INT(10),
@@ -25,16 +25,16 @@ CREATE TABLE sustaina_db.ssyc22_teachers (
 	account_enabled BOOLEAN DEFAULT 1
 ) ENGINE = InnoDB;
 
-CREATE TABLE sustaina_db.ssyc22_students (
+CREATE TABLE sustaina_db.ssys22_students (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	first_name CHAR(20) NOT NULL,
-    last_name CHAR(20) NOT NULL,
+	fname CHAR(20) NOT NULL,
+    lname CHAR(20) NOT NULL,
     email CHAR(40) NOT NULL,
 	email_confirmed BOOLEAN DEFAULT 0,
 	confirmation_code INT(10),
 	teacher_email CHAR(40),
 	teacher_id INT,
-	FOREIGN KEY (teacher_id) REFERENCES ssyc22_teachers(id),
+	FOREIGN KEY (teacher_id) REFERENCES ssys22_teachers(id),
 	password_set BOOLEAN DEFAULT 0,
     password_hash CHAR(200),
 	grade int(5),
@@ -56,14 +56,14 @@ CREATE TABLE sustaina_db.ssyc22_students (
 	account_enabled BOOLEAN DEFAULT 1
 ) ENGINE = InnoDB;
 
-CREATE TABLE sustaina_db.ssyc22_admin (
+CREATE TABLE sustaina_db.ssys22_admin (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	username CHAR(50) NOT NULL,
 	email CHAR(40) NOT NULL,
 	password_hash CHAR(200) NOT NULL
 ) ENGINE = InnoDB;
 
-CREATE TABLE sustaina_db.ssyc22_settings (
+CREATE TABLE sustaina_db.ssys22_settings (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	registration_status INT NOT NULL,
 	num_attendees INT NOT NULL,
