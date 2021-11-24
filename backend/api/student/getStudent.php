ebc52af3289a8ee702da6bc4d68bc20b14ee3f1d
Zac Waite <129032699zw@gmail.com>
2021-11-23 19:44:22 -0500

    Basic get endpoint

--- backend/api/student/getStudent.php
+++ backend/api/student/getStudent.php
@@ -1,3 +1,37 @@
 <?php
-echo 'Hello ' . htmlspecialchars($_GET["username"]) . '!';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+if (file_exists(__DIR__."/../../vendor/autoload.php")
+&& file_exists(__DIR__."/../../modules/database.php")
+&& file_exists(__DIR__."/../../modules/env/.env")
+&& file_exists(__DIR__."/../../models/response.php")
+&& file_exists(__DIR__."/../../models/student/getStudent.php")){
+	//Imports
+	require_once __DIR__."/../../vendor/autoload.php";
+	require_once __DIR__."/../../models/response.php";
+	require_once __DIR__."/../../modules/database.php"; //Connect to database
+	require_once __DIR__."/../../models/student/getStudent.php";
+
+	$dotenv = new Dotenv();
+	$dotenv->load(__DIR__."/../../modules/env/.env");
+
+	//Main
+	#Object declaraion
+	$res = new Response();
+
+	if (count($res->errors)==0){
+		$results = DB::query("SELECT fname, lname, email FROM ssys22_students");
+		foreach ($results as $row) {
+			echo "fname: " . $row['fname'] . "\n";
+			echo "lname: " . $row['lname'] . "\n";
+			echo "email: " . $row['email'] . "\n";
+			echo "-------------\n";
+		}
+		$res->status = 200;
+		$res->success = true;
+	}
+	echo json_encode($res);
+} else {echo "Import error";}
 ?>
+
---
+++ backend/models/student/getStudent.php
@@ -0,0 +1,6 @@
+<?php
+function parseStudentData($param){
+	// if (isset($_POST[$param])) return htmlspecialchars(stripslashes(trim($_POST[$param])));
+	// else return false;
+}
+?>
