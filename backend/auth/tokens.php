233de127a1ed40180e2b920ff5c4b398bb14a048
Zac Waite <129032699zw@gmail.com>
2021-11-24 19:24:59 -0500

    Token functions

---
+++ backend/auth/tokens.php
@@ -0,0 +1,37 @@
+<?php
+use Symfony\Component\Dotenv\Dotenv;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+header('Content-Type:application/json; charset=utf-8');
+
+if (file_exists(__DIR__."/../vendor/autoload.php")
+&& file_exists(__DIR__."/../modules/env/.env")){
+	//Imports
+	require_once __DIR__."/../vendor/autoload.php";
+	$dotenv = new Dotenv();
+	$dotenv->load(__DIR__."/../modules/env/.env");
+
+	function createToken ($body) {
+		$key = $_ENV['JWT_KEY'];
+		$token = JWT::encode($body, $key, 'HS256');
+		return $token;
+	}
+	function verifyToken ($token) {
+		$key = $_ENV['JWT_KEY'];
+		try {
+			$decoded = JWT::decode($token, new Key($key, 'HS256'));
+			return $decoded;
+		} catch (Exception $e) {
+			return false;
+		}
+	}
+	function getToken (){
+		$auth = getallheaders()["Authorization"];
+		$token = explode(" ", $auth)[1]; //Got to love php's string split being EXPLODE
+		$token = str_replace("\"", "",$token);
+		if ($token) return $token;
+		else return false;
+	}
+} else {echo "Import error";}
+//getToken from auth
+?>
