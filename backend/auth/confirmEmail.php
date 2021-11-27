c4ffd912c8da4ea0b8292d758c9b220eeb0b53a4
Zac Waite <129032699zw@gmail.com>
2021-11-27 00:02:57 -0500

    Email confirmation

--- backend/auth/confirmEmail.php
+++ backend/auth/confirmEmail.php
@@ -14,39 +14,48 @@ if (file_exists(__DIR__."/../vendor/autoload.php")
 	require_once __DIR__."/../modules/readParams.php";
 	require_once __DIR__."/tokens.php";
 
-	$dotenv = new Dotenv();
-
 	//Main
 	#Object declaraion
 	$res = new Response();
 
-	$email = getBody("email");
-	$password = getBody("password");
+	$email = getQuery("email");
+	$code = getBody("confirmation_code");
 	if(!$email) array_push($res->errors, "missing email query");
-	if(!$password) array_push($res->errors, "missing password query");
+	if(!$code) array_push($res->errors, "missing confirmation_code in body");
 	if (count($res->errors)==0){
-		$query = "id, password_hash";
-		$teacherResult = DB::queryFirstRow("SELECT ".$query." FROM ssys22_teachers WHERE email=%s LIMIT 1", $email);
-		$studentResult = DB::queryFirstRow("SELECT ".$query." FROM ssys22_students WHERE email=%s LIMIT 1", $email);
+		$query = "id, confirmation_code";
+		$teacherResult = DB::queryFirstRow("SELECT ".$query." FROM ssys22_teachers WHERE email=%sLIMIT 1", $email);
+		$studentResult = DB::queryFirstRow("SELECT ".$query." FROM ssys22_students WHERE email=%sLIMIT 1", $email);
 		$success = false;
 		if (boolval($teacherResult['id'])) {
-			$hash = $teacherResult['password_hash'];
-			$success = password_verify($password, $hash);
+			if ($teacherResult['confirmation_code']==$code){
+				$puts = array();
+				$puts['email_confirmed'] = true;
+				DB::update('ssys22_teachers', $puts, "email=%s", $email);
+				$success = true;
+			} else {
+				$success = false;
+				array_push($res->errors, "incorrect code");
+			}
 		} else if (boolval($studentResult['id'])){
-			$hash = $studentResult['password_hash'];
-			$success = password_verify($password, $hash);
+			if ($studentResult['confirmation_code']==$code){
+				$puts = array();
+				$puts['email_confirmed'] = true;
+				DB::update('ssys22_students', $puts, "email=%s", $email);
+				$success = true;
+			} else {
+				$success = false;
+				array_push($res->errors, "incorrect code");
+			}
 		} else {
 			$res->status = 404;
 			array_push($res->errors, "account not found");
 		}
 		if ($success) {
-			$tokenBody = new tokenBody($email);
-			$token = createToken($tokenBody);
 			$res->status = 200;
 			$res->success = true;
-			$res->objects = $token;
 		} else {
-			array_push($res->errors, "signin failed");
+			array_push($res->errors, "confirmation failed");
 		}
 	}
 	echo json_encode($res);
