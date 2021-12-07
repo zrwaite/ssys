534c64cf1f46f429c51ec1a41621106a8a22aca4
Zac Waite <129032699zw@gmail.com>
2021-12-07 14:00:47 -0500

    Removed forced imports

--- backend/api/teacher/getTeacher.php
+++ backend/api/teacher/getTeacher.php
@@ -1,43 +1,34 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+require_once __DIR__."/../../vendor/autoload.php";
+require_once __DIR__."/../../models/response.php";
+require_once __DIR__."/../../modules/database.php"; //Connect to database
+require_once __DIR__."/../../modules/readParams.php";
+require_once __DIR__."/../../models/teacher.php";
 
-if (file_exists(__DIR__."/../../vendor/autoload.php")
-&& file_exists(__DIR__."/../../modules/database.php")
-&& file_exists(__DIR__."/../../modules/env/.env")
-&& file_exists(__DIR__."/../../models/response.php")
-&& file_exists(__DIR__."/../../modules/readParams.php")
-&& file_exists(__DIR__."/../../models/teacher.php")){
-	//Imports
-	require_once __DIR__."/../../vendor/autoload.php";
-	require_once __DIR__."/../../models/response.php";
-	require_once __DIR__."/../../modules/database.php"; //Connect to database
-	require_once __DIR__."/../../modules/readParams.php";
-	require_once __DIR__."/../../models/teacher.php";
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__."/../../modules/env/.env");
 
-	$dotenv = new Dotenv();
-	$dotenv->load(__DIR__."/../../modules/env/.env");
-
-	//Main
-	#Object declaraion
-	$res = new Response();
-	$teacher = new GetTeacher();
-	$email = getQuery("email");
-	if(!$email) array_push($res->errors, "missing email query");
-	if (count($res->errors)==0){
-		$query = "id, fname, lname, email, email_confirmed, image_link, school ,shirt_size ,shirts_ordered ,city ,workshop_choices ,diet ,workshop_order ,video_link ,video_approved ,bio ,additional_info ,account_enabled";
-		$result = DB::queryFirstRow("SELECT ".$query." FROM ssys22_teachers WHERE email=%s LIMIT 1", $email);
-		if($teacher->parseResult($result)){
-			$res->status = 200;
-			$res->success = true;
-			$res->objects = $teacher;
-		} else {
-			$res->status = 404;
-			array_push($res->errors, "teacher not found");
-		}
+//Main
+#Object declaraion
+$res = new Response();
+$teacher = new GetTeacher();
+$email = getQuery("email");
+if(!$email) array_push($res->errors, "missing email query");
+if (count($res->errors)==0){
+	$query = "id, fname, lname, email, email_confirmed, image_link, school ,shirt_size ,shirts_ordered ,city ,workshop_choices ,diet ,workshop_order ,video_link ,video_approved ,bio ,additional_info ,account_enabled";
+	$result = DB::queryFirstRow("SELECT ".$query." FROM ssys22_teachers WHERE email=%s LIMIT 1", $email);
+	if($teacher->parseResult($result)){
+		$res->status = 200;
+		$res->success = true;
+		$res->objects = $teacher;
+	} else {
+		$res->status = 404;
+		array_push($res->errors, "teacher not found");
 	}
-	http_response_code($res->status);
-	echo json_encode($res);
-} else {echo "Import error";}
+}
+http_response_code($res->status);
+echo json_encode($res);
 ?>
 
--- backend/api/teacher/postTeacher.php
+++ backend/api/teacher/postTeacher.php
@@ -1,70 +1,63 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
+//Imports
+require_once __DIR__."/../../vendor/autoload.php";
+require_once __DIR__."/../../models/response.php";
+require_once __DIR__."/../../modules/database.php"; //Connect to database
+require_once __DIR__."/../../modules/readParams.php";
+require_once __DIR__."/../../models/teacher.php";
 
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__."/../../modules/env/.env");
 
-if (file_exists(__DIR__."/../../vendor/autoload.php")
-&& file_exists(__DIR__."/../../modules/database.php")
-&& file_exists(__DIR__."/../../modules/env/.env")
-&& file_exists(__DIR__."/../../models/response.php")
-&& file_exists(__DIR__."/../../modules/readParams.php")
-&& file_exists(__DIR__."/../../models/teacher.php")){
-	//Imports
-	require_once __DIR__."/../../vendor/autoload.php";
-	require_once __DIR__."/../../models/response.php";
-	require_once __DIR__."/../../modules/database.php"; //Connect to database
-	require_once __DIR__."/../../modules/readParams.php";
-	require_once __DIR__."/../../models/teacher.php";
+//Main
+//Object declaraion
+$res = new Response();
+$teacher = new PostTeacher();
 
-	$dotenv = new Dotenv();
-	$dotenv->load(__DIR__."/../../modules/env/.env");
-
-	//Main
-	//Object declaraion
-	$res = new Response();
-	$teacher = new PostTeacher();
-
-	//get post queries
-	$email = getBody("email");
-	if($email) $teacher->email = $email;
-	else array_push($res->errors, "Must include email");
+//get post queries
+$email = getBody("email");
+if($email) {
+	$teacher->email = $email;
 	if(!$teacher->checkEmail()) array_push($res->errors, "Invalid email");
-	
-	$fname = getBody("fname");
-	if ($fname) $teacher->fname = $fname;
-	else array_push($res->errors, "Must include fname");
+}
+else array_push($res->errors, "Must include email");
 
-	$lname = getBody("lname");
-	if ($lname) $teacher->lname = $lname;
-	else array_push($res->errors, "Must include lname");
+$fname = getBody("fname");
+if ($fname) $teacher->fname = $fname;
+else array_push($res->errors, "Must include fname");
 
-	$password = getBody("password");
-	if ($password) $teacher->password = $password;
-	else array_push($res->errors, "Must include password");
-	$res->errors = array_merge($res->errors, $teacher->checkPassword());
-	if (count($res->errors)==0){
-		$teacher->createHash();
-		try {
-			$result = DB::queryFirstRow("SELECT id FROM ssys22_teachers WHERE email=%s LIMIT 1", $email);
-			if (!$result){
-				DB::insert('ssys22_teachers', array(
-					'email' => $teacher->email,
-					'fname' => $teacher->fname,
-					'lname' => $teacher->lname,
-					'password_hash' => $teacher->hash,
-					'confirmation_code' => $teacher->confirmation_code
-				));
-				$res->status = 200;
-				$res->success = true;
-				//send email with link to confirm email page with confirmation_code
-			} else {
-				array_push($res->errors, "Email already in use");
-			}
-		} catch (Exception $e) {
-			echo 'Message: ' .$e->getMessage();
+$lname = getBody("lname");
+if ($lname) $teacher->lname = $lname;
+else array_push($res->errors, "Must include lname");
+
+$password = getBody("password");
+if ($password) $teacher->password = $password;
+else array_push($res->errors, "Must include password");
+$res->errors = array_merge($res->errors, $teacher->checkPassword());
+if (count($res->errors)==0){
+	$teacher->createHash();
+	try {
+		$result = DB::queryFirstRow("SELECT id FROM ssys22_teachers WHERE email=%s LIMIT 1", $email);
+		if (!$result){
+			DB::insert('ssys22_teachers', array(
+				'email' => $teacher->email,
+				'fname' => $teacher->fname,
+				'lname' => $teacher->lname,
+				'password_hash' => $teacher->hash,
+				'confirmation_code' => $teacher->confirmation_code
+			));
+			$res->status = 200;
+			$res->success = true;
+			//send email with link to confirm email page with confirmation_code
+		} else {
+			array_push($res->errors, "Email already in use");
 		}
+	} catch (Exception $e) {
+		echo 'Message: ' .$e->getMessage();
 	}
-	http_response_code($res->status);
-	echo json_encode($res);
-} else {echo "Import error";}
+}
+http_response_code($res->status);
+echo json_encode($res);
 ?>
