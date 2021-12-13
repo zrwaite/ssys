be529278e55f97528a43f6c4d87353d035ed4d12
Zac Waite <129032699zw@gmail.com>
2021-12-13 14:50:58 -0500

    Delete endpoint complete

--- backend/api/student/deleteStudent.php
+++ backend/api/student/deleteStudent.php
@@ -8,6 +8,7 @@ require_once __DIR__ . "/../../models/response.php";
 require_once __DIR__ . "/../../modules/database.php"; //Connect to database
 require_once __DIR__ . "/../../modules/readParams.php";
 require_once __DIR__ . "/../../models/student.php";
+require_once __DIR__ . "/../../modules/checkers.php";
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . "/../../modules/env/.env");
@@ -15,13 +16,20 @@ $dotenv->load(__DIR__ . "/../../modules/env/.env");
 $res = new Response();
 $res->request_type = "DELETE";
 
-$email = getQuery("email");
+$email = getBody("email");
+if (!$email) array_push($res->errors, "missing email");
+$password = getBody("password");
+if (!$password) array_push($res->errors, "missing password");
 
-if (!$email) array_push($res->errors, "missing email query");
 if (count($res->errors) == 0) {
-    $result = DB::queryFirstRow("SELECT id FROM ssys22_students WHERE email=%s LIMIT 1", $email);
-    //TODO implement delete request
+    $res->status = validatePassword($password, $email);
+    if ($res->status == 200) {
+        DB::delete('ssys22_students', 'email=%s', $email);
+        $res->objects = "deleted";
+        $res->success = true;
+    } else if ($res->status == 400) array_push($res->errors, "incorrect password");
+    else if ($res->status == 404) array_push($res->errors, "account not found");
 }
 
-
-echo "Zac hasn't implemented delete yet, deal with it";
+http_response_code($res->status);
+echo json_encode($res);
--- backend/api/teacher/deleteTeacher.php
+++ backend/api/teacher/deleteTeacher.php
@@ -11,8 +11,23 @@ require_once __DIR__ . "/../../models/teacher.php"; //Teacher api class
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . "/../../modules/env/.env");
 
-echo "Zac hasn't implemented delete yet, deal with it";
-//Main
-#Object declaraion
 $res = new Response();
 $res->request_type = "DELETE";
+
+$email = getBody("email");
+if (!$email) array_push($res->errors, "missing email");
+$password = getBody("password");
+if (!$password) array_push($res->errors, "missing password");
+
+if (count($res->errors) == 0) {
+    $res->status = validatePassword($password, $email);
+    if ($res->status == 200) {
+        DB::delete('ssys22_teachers', 'email=%s', $email);
+        $res->objects = "deleted";
+        $res->success = true;
+    } else if ($res->status == 400) array_push($res->errors, "incorrect password");
+    else if ($res->status == 404) array_push($res->errors, "account not found");
+}
+
+http_response_code($res->status);
+echo json_encode($res);
