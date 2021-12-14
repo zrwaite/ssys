adb6641a0827b372bcacad122fb89d424bf9e051
Zac Waite <129032699zw@gmail.com>
2021-12-13 19:59:17 -0500

    CORS bypass

--- backend/modules/mysqltest.php
+++ backend/modules/mysqltest.php
@@ -1,10 +1,16 @@
 <?php
+header('Access-Control-Allow-Origin: *'); //dev allow localhost cors
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 //IMPORTS
 require __DIR__ . '/database.php';
 
 //MAIN
+$ids = array();
 $accounts = DB::query("SELECT id FROM ssys22_students");
 foreach ($accounts as $account) {
-    echo $account['id'] . "\n";
+    array_push($ids, $account['id']);
 }
-echo "Connected successfully";
+http_response_code(400);
+
+echo json_encode(["ids" => $ids, "success" => true]);
