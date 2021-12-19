42865bd5805f8493b308a2c4da7a29cbaf336d91
weiqixu05 <weiqixu2005@icloud.com>
2021-12-19 01:47:19 -0500

    Made register choice look better

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,6 +3,7 @@ import "../styles/styles.css";
 import StudentRegistration from "../components/StudentRegistration";
 import IndividualRegistration from "../components/IndividualRegistration";
 import TeacherRegistration from "../components/TeacherRegistration";
+import "../styles/register.css";
 
 function Register() {
     let [registerDisplay, setRegisterDisplay] = useState(null);
@@ -11,22 +12,20 @@ function Register() {
             <header>
                 <h1>Register Page</h1>
             </header>
-            <h3>Register as:</h3>
-            <table>
-                <tbody>
-                <tr>
-                    <td>
+            <div class="register_options">
+                <div class="one">
+                    <h3>Register as:</h3>                
+                </div>
+                <div class="two">
                         <button onClick={() => setRegisterDisplay("student")}>Student</button>
-                    </td>
-                    <td>
+                </div>
+                <div class="three">
                         <button onClick={() => setRegisterDisplay("individual")}>Individual</button>
-                    </td>
-                    <td>
+                </div>
+                <div class="four">
                         <button onClick={() => setRegisterDisplay("teacher")}>Teacher</button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+                </div>
+            </div>
             <StudentRegistration display={registerDisplay}/>
             <IndividualRegistration display={registerDisplay}/>
             <TeacherRegistration display={registerDisplay}/>
