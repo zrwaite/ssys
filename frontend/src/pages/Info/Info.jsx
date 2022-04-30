68d9dcc245382e123fd5d2ce2cec91f911232300
weiqixu05 <weiqixu2005@icloud.com>
2022-04-30 11:24:07 -0400

    changes to info

--- frontend/src/pages/Info/Info.jsx
+++ frontend/src/pages/Info/Info.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Info.css";
+import lakehead from "../../components/lakehead.png";
 import diannesaxe from "../../components/diannesaxe.jpg";
 import {Link} from "react-router-dom";
 
@@ -15,14 +16,24 @@ function Info() {
             <div className="infoWho">
                 <h1>WHO?</h1>
                 <div className="infoWhoBlurb">
-                    <p>200 HIGH SCHOOL STUDENTS FROM ACROSS SIMCOE COUNTY. 10 WORKSHOP LEADERS. 2 KEYNOTE SPEAKERS. <br/>
+                    <p>200 HIGH SCHOOL STUDENTS FROM ACROSS SIMCOE COUNTY. 10 WORKSHOP LEADERS. 2 KEYNOTE SPEAKERS. <br/><br/>
                     FOR MORE INFORMATION ON THE WORKSHOPS, CLICK THE BUTTON BELOW</p>
+                </div>
+                <div className="infoWhoButtonBox">
+                    <Link to="/workshop" className="infoWhoButton">
+                           <span>Workshops</span>
+                    </Link>
                 </div>
             </div>
             <div className="infoWhere">
                 <h1>WHERE?</h1>
-                <div className="infoWhereBlurb">
-                    <p>Lakehead University Orillia Campus, 500 University Ave, Orillia</p>
+                <div className="infoWhereGrid">
+                    <div className="infoWhereImageBox">
+                         <img className = "infoWhereImage" src={lakehead} alt="lakehead" />
+                    </div>
+                    <div className="infoWhereBlurb">
+                        <p>Lakehead University Orillia Campus, 500 University Ave, Orillia</p>
+                    </div>
                 </div>
             </div>
             
@@ -31,3 +42,4 @@ function Info() {
 } 
 
 export default Info;
+
