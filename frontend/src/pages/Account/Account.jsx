17e3736ba2001117df90e5e0513f8e9949e593f5
weiqixu05 <weiqixu2005@icloud.com>
2022-04-29 21:54:53 -0400

    minor changes to account css

--- frontend/src/pages/Account/Account.jsx
+++ frontend/src/pages/Account/Account.jsx
@@ -15,13 +15,12 @@ function Account() {
 
     return (
         <main>
-            <div className="top">
+            <div className="accTop">
                 <h1>REGISTER/SIGN IN</h1>
                 <div className="accBlurb">
                     <p>Make sure you don't miss out on this incredible opportunity! Register now before it's too late!</p>
                 </div>
             </div>
-            <p><br/></p>
             <div className="acc">
               <div className="accReg">
                          <Link to="/register" className="accButton">
