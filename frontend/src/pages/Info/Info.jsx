e30aeb98d3ff3be481df1142c53866058bee1cda
weiqixu05 <weiqixu2005@icloud.com>
2022-04-30 14:30:48 -0400

    Info page almost complete?

--- frontend/src/pages/Info/Info.jsx
+++ frontend/src/pages/Info/Info.jsx
@@ -78,7 +78,9 @@ function Info() {
                          <img className = "infoDibbleImage" src={BradDibble} alt="Dr. Brad Dibble" />
                     </div>
                     <div className="infoDibbleBlurb">
-                        <p>Dr. Brad Dibble was appointed by the federal Minister of the Environment as one of 25 members to the Sustainable Development Advisory Council, a membership he still maintains. A Mentor for Climate Reality Canada, he has received training with Al Gore and endorsements from Col. Chris Hadfield for his work.  He has also written the award-winning book, Comprehending the Climate Crisis: Everything you need to know about Global Warming and how to stop it (2012).  </p>
+                        <p>Dr. Brad Dibble was appointed by the federal Minister of the Environment as one of 25 members to the Sustainable Development Advisory Council, a membership he still maintains. A Mentor for Climate Reality Canada, he has received training with Al Gore and endorsements from Col. Chris Hadfield for his work.  He has also written the award-winning book, Comprehending the Climate Crisis: Everything you need to know about Global Warming and how to stop it (2012).  
+                            <br/><br/> His opening keynote address will provide summit participants with all the background information they need to understand the science behind the climate crisis, and what changes need to be made to stop the damage before it is too late. 
+                        </p>
                     </div>
                 </div>
             </div>
@@ -89,8 +91,16 @@ function Info() {
                          <img className = "infoAllieImage" src={AllieRougeot} alt="Aliénor (Allie) Rougeot" />
                     </div>
                     <div className="infoAllieBlurb">
-                        <p>Allie Rougeot is an Economics and Public Policy student at the University of Toronto. She has been an environmental and human rights activist since the age of 10, and has, since high school, been passionate about sustainability and climate justice. Since January 2019 she has been the lead coordinator of the Fridays for Future movement in Toronto. She has been recognized for her work by the Canadian Voice of Women for peace with the Kim Phuc Youth Award, and has appeared several times in the Toronto Star, on Metro Morning, CTV, Global News, Radio Canada, National Observer and other media.</p>
+                        <p>Allie Rougeot is an Economics and Public Policy student at the University of Toronto. She has been an environmental and human rights activist since the age of 10, and has, since high school, been passionate about sustainability and climate justice. Since January 2019 she has been the lead coordinator of the Fridays for Future movement in Toronto. She has been recognized for her work by the Canadian Voice of Women for peace with the Kim Phuc Youth Award, and has appeared several times in the Toronto Star, on Metro Morning, CTV, Global News, Radio Canada, National Observer and other media.
+                            <br/><br/>Her closing keynote address will focus on her experiences as a youth organizer, activist and changemaker. She is here to inspire and empower summit participants to take what they learned throughout the day and use it to create real change in their communities, and fight for the future in which they want to live!
+                        </p>
                     </div>
+                </div>
+            </div>
+            <div className="infoBring">
+                <h1>What to bring?</h1>
+                <div className="infoBringBlurb">
+                    <p>As per Lakehead University, masks are mandatory so please bring a mask! Lunch is provided by Lakehead University. Any dietary restrictions must be communicated through schools or through the account page on the website. We will try our best to accommodate your request. Our complimentary “Swag bags” will include a pen, notebook, a drawstring bag, and a reusable water bottle. Snacks will also be included in the bags. Please leave valuables at home and only bring what you absolutely need. There will be separate places for schools/groups to put their things if needed. </p>
                 </div>
             </div>
         </main>
