442c64d2c990b9e22e27d969212d0d2eb35fc693
weiqixu05 <weiqixu2005@icloud.com>
2022-05-01 15:29:56 -0400

    changes to workshops

--- frontend/src/pages/Workshop/Workshop.jsx
+++ frontend/src/pages/Workshop/Workshop.jsx
@@ -10,6 +10,7 @@ import YCL from "../../components/YCL.jpg";
 import BradDibble from "../../components/BradDibble.jpg";
 import Ellen from "../../components/Ellen.jpg";
 import Bolu from "../../components/Bolu.jpg";
+import Alyvia from "../../components/Alyvia.png";
 import {Link} from "react-router-dom";
 
 const workshopsData = [
@@ -65,16 +66,23 @@ const workshopsData = [
     {
         name: "Boluwatife Ogunniyi",
         imageSrc: Bolu,
-        title: "Workshop: “Power of Youth: Leading a Youth Climate Group”",
-        summary: "filler",
-        description: "Filler",
+        title: "Workshop: “Power of Youth: Grassroots Youth Environmental Action”",
+        summary: "Bolu Ogunniyi is a student at the University of Toronto, hoping to pursue a double major in Global Health and Bioethics...",
+        description: "Bolu Ogunniyi is a student at the University of Toronto, hoping to pursue a double major in Global Health and Bioethics. She is also the creator and former president of the Simcoe County Environmental Youth Alliance (SCEYA). She has much experience in advocacy and youth organizing and is passionate about building a better future for youth to come. In Bolu’s workshop she will walk students through the ins and outs of youth organizing and creating and managing youth environmentalism groups. She will show students just how possible it is to run successful initiatives as a young person in Simcoe County, and give them tips and tricks on how to accomplish these feats in their own communities. Students will then get to share their experiences within youth climate action and Bolu will share her experiences and answer questions in turn.",
+    },
+    {
+        name: "Alyvia Sipidias",
+        imageSrc: Alyvia,
+        title: "Workshop: “Climate Action Planning: Where to Begin”",
+        summary: "Alyvia Sipidias is a certified teacher (OCT) and graduate student in the Masters of Education program...",
+        description: "Alyvia Sipidias is a certified teacher (OCT) and graduate student in the Masters of Education program, specialization in Education for Change: Environmental Sustainability Education at Lakehead University. In this workshop Alyvia will provide students with an action-planning framework for students to consolidate their learning from the one day summit into an action plan for their home school. Through her graduate assistantship Alyvia is working with the Community Engagement and Lifelong Learning department. Based on her passion for environmental education, Alyvia will be assisting the Sustainable Orillia Youth Council with the day long Summit event. In this workshop, Alyvia will be teaching an introductory class to climate action planning, teaching students the simple things they need in order to get started on their climate action journey.",
     },
     {
         name: "Youth Climate Lab",
         imageSrc: YCL,
         title: "Workshop: “Responsive Education”",
-        summary: "filler",
-        description: "Filler",
+        summary: "The Youth Climate Lab is a youth-led organization that focuses on enabling youth to re-imagine and rebuild...",
+        description: "The Youth Climate Lab is a youth-led organization that focuses on enabling youth to re-imagine and rebuild the flawed systems holding in place climate inaction, so that youth can achieve a just, climate-resilient world.  Through their pop-up climate labs, they facilitate projects that focus on driving shifts that enable youths to gain the skills to be creative leaders and radical collaborators that can build policy solutions that challenge current systems of thought and societal organization. The Youth Climate Lab has collaborated with 30+ partners around the globe to reach youth in 77 different countries. In this workshop, the Youth Climate Lab will ask students to discuss experiences with teachers and adult allies that have allowed for them to learn about and take action on climate change. Student responses along with a follow-up debrief workshop with youth organizers will inform the development of a model of the roles and processes of responsive education, that is education approaches that address the specific positionality of how youth will be adversely affected by climate impacts but currently do not have agency to control policy that will affect their future quality of life. This workshop will replace the ‘fourth’ workshop time described in the itinerary - all students will participate in their class groups. The goal of this workshop is to leave students with a tangible plan for a climate action initiative they can bring back to their home communities (school, greater community, etc.) The team at the Sustainable Orillia Youth Council found that when they attended conferences such as the summit, they would leave incredibly inspired but without a plan to turn that inspiration into action. We wish to bring and end to that, and the Youth Climate Lab workshop will ensure that students leave with the framework for a climate action initiative. ",
     },
 ]
 
@@ -130,7 +138,7 @@ function Workshop() {
     }
     })
 }
-    <div className="workshopBlue" id="teacherWorkshop">
+    <div className="workshopGreen" id="teacherWorkshop">
         <h1>Dr. Ellen Field and Bonnie Anderson</h1>
         <div className="workshopGrid">
             <div className="workshopImageBox">
@@ -140,7 +148,8 @@ function Workshop() {
                 <p>Parallel Professional Development Workshop for Teachers - Integrating Climate Change into Your Teaching</p><br/><br/>
                 <details>
                 <summary>Ellen Field is an Assistant Professor in the Faculty of Education at Lakehead University...</summary>
-                <p>Ellen Field is an Assistant Professor in the Faculty of Education at Lakehead University. Her research interests are in policy and practice of climate change education in the Canadian K-12 system. Dr. Field has a strong background in survey development and implementation, educational tool development and stakeholder engagement. She is engaged with Ministries, school boards, teachers and community members, and co-leads an online community on Climate Change Education in Canada. Ellen teaches Environmental Education (B.Ed) and Climate Change Education (M.Ed) in the Faculty of Education, and has engaged 800 teachers in professional development workshops in the last several years. Ellen is an Associate Editor of the Canadian Journal of Environmental Education and co-chair of the Canadian Regional Hub of Monitoring and Evaluation of Climate Change Education (MECCE). Bonnie Anderson is the Outdoor Environmental Education and Healthy Active Living Coordinator for the Simcoe County District School Board. She has extensive experience in leadership in environmental education programs and has worked in Peel, Toronto, Bark Lake Leadership Centre, and Kawartha Pine Ridge District School Boards in coordinating positions.</p>
+                <p>Ellen Field is an Assistant Professor in the Faculty of Education at Lakehead University. Her research interests are in policy and practice of climate change education in the Canadian K-12 system. Dr. Field has a strong background in survey development and implementation, educational tool development and stakeholder engagement. She is engaged with Ministries, school boards, teachers and community members, and co-leads an online community on Climate Change Education in Canada. Ellen teaches Environmental Education (B.Ed) and Climate Change Education (M.Ed) in the Faculty of Education, and has engaged 800 teachers in professional development workshops in the last several years. Ellen is an Associate Editor of the Canadian Journal of Environmental Education and co-chair of the Canadian Regional Hub of Monitoring and Evaluation of Climate Change Education (MECCE). Bonnie Anderson is the Outdoor Environmental Education and Healthy Active Living Coordinator for the Simcoe County District School Board. She has extensive experience in leadership in environmental education programs and has worked in Peel, Toronto, Bark Lake Leadership Centre, and Kawartha Pine Ridge District School Boards in coordinating positions. While SSYS students will be partaking in multiple workshop sessions throughout the duration of the summit, a day-long parallel professional development session will be running for educators accompanying their students. The workshop is designed around professional inquiry, in which teachers will co-learn through interactive activities along three lines of inquiry: 1) climate change risks and climate solutions, 2) transformative and transgressive pedagogies and teaching strategies for 21st century climate change education, and 3) building effective understanding into climate change teaching practices. The parallel session will also help address the need to provide professional development for teachers on climate change education. In the national survey, Canada, Climate Change and Education: Opportunities for Public and Formal Education, results report that the average amount of instruction a student in grade 7 -12 year receives in a year or semester is 1 to 10 hours; that 50% of teachers indicated that professional development would help them integrate climate change education in their teaching, and fewer than half of teachers indicated that they feel they have the knowledge and skills to teach about climate change (Field, Schwartzberg, Berger, 2019). The parallel session will provide teachers with the tools needed to successfully teach their students about climate change and climate action, as well as support them in their climate action initiatives. 
+                </p>
                 </details>
             </div>
         </div>
