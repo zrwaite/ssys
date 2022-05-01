224e1a4ff55426275c5453600778b84f13e2c0b0
weiqixu05 <weiqixu2005@icloud.com>
2022-05-01 17:06:23 -0400

    Changes to Teacher Code

--- frontend/src/pages/TeacherCode/TeacherCode.jsx
+++ frontend/src/pages/TeacherCode/TeacherCode.jsx
@@ -4,7 +4,12 @@ import "./TeacherCode.css";
 function TeacherCode() {
     return(
     <main>
-        
+        <div className="teacherCodeTop">
+            <h1>Blythe's Page</h1>
+            <div className="teacherCodeBlurb">
+            <p>This is a page for Blythe. If you are not Blythe, then this page is not for you.</p>
+        </div>
+        </div>
     </main>
     );
 }
