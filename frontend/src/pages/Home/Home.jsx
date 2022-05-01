74b8c854bacb0d997d5138cbb2c67ea4a9e6c6e9
weiqixu05 <weiqixu2005@icloud.com>
2022-04-30 20:53:27 -0400

    done homepage?

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import logo from "../../components/SOYC.png";
+import LindaEllen from "../../components/LindaEllen.jpg";
 import "./Home.css";
 import CountdownRegistration from "../../components/CountdownRegistration/CountdownRegistration.jsx";
 import CountdownSummit from "../../components/CountdownSummit/CountdownSummit.jsx";
@@ -39,6 +40,37 @@ function Home() {
                     <Link to="/info" className="homeRegister">
                            <span>Info</span>
                     </Link>
+                </div>
+            </div>
+            <div className="homeBlue">
+                <h1>Meet the Organizers</h1>
+                <div className="SOYCGrid">
+                    <div className="SOYCImageBox">
+                         <img className = "SOYCImage" src={logo} alt="Sustainable Orillia Youth Council" />
+                    </div>
+                    <div className="SOYCBlurb">
+                        <p>The Sustainable Orillia Youth Council
+                            <br/><br/>The Sustainable Orillia Youth Council (SOYC) is made up of high school students from across Orillia and surrounding area. Our members are passionate about making a difference in our community by spreading awareness about sustainability and creating initiatives to better the sustainability of Orillia and its residents (such as this one)! The SOYC worked hard to organize this summit. For more information about the SOYC, click the button below.
+                        </p>
+                        <div className="SOYCButtonBox">
+                            <a href='https://www.sustainableorillia.ca/youth-council'>
+                                <button className="SOYCButton">SOYC</button>   
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="homeGreen">
+                <h1>Meet the Organizers (Part 2)</h1>
+                <div className="SOYCGrid">
+                    <div className="SOYCImageBox">
+                         <img className = "SOYCImage" src={LindaEllen} alt="Dr. Ellen Field and Dr. Linda Rodenburg" />
+                    </div>
+                    <div className="SOYCBlurb">
+                        <p>Dr. Ellen Field (left) and Dr. Linda Rodenburg (right)
+                            <br/><br/>Dr. Ellen Field and Dr. Linda Rodenburg are both professors at Lakehead University. Dr. Field’s research is focused on the policy and practice of climate change education within the k-12 school system. Dr. Rodenburg specializes in postcolonial literatures with a particular interest in indigenous cultural theory. They both have provided us with great support throughout the journey of planning this summit.
+                        </p>
+                    </div>
                 </div>
             </div>
         </main>
