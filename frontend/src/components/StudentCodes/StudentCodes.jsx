eb18019fd6fb0999509f826b880b0598163c9544
Zac Waite <129032699zw@gmail.com>
2022-05-01 19:01:04 -0400

    Fix codes

--- frontend/src/components/StudentCodes/StudentCodes.jsx
+++ frontend/src/components/StudentCodes/StudentCodes.jsx
@@ -23,10 +23,11 @@ const StudentCodes = () => {
         let json = await httpReq("/api/codes/", "POST", {
             username: getCookie("username"),
             num_codes: state.numCodes,
+            teacher: false
         })
         let response = JSON.parse(json);
         if (response.success && response.objects) {
-            setCodes(...codes, response.objects)
+            setCodes([...codes, ...response.objects])
         } else if (response.errors.length > 0) {
             alert(JSON.stringify(response.errors));
         }
