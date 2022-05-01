786c59f01f8604ef782bba964ac91c568552a134
weiqixu05 <weiqixu2005@icloud.com>
2022-05-01 17:16:19 -0400

    changes to TeacherCode

--- frontend/src/pages/TeacherCode/TeacherCode.jsx
+++ frontend/src/pages/TeacherCode/TeacherCode.jsx
@@ -7,8 +7,17 @@ function TeacherCode() {
         <div className="teacherCodeTop">
             <h1>Blythe's Page</h1>
             <div className="teacherCodeBlurb">
-            <p>This is a page for Blythe. If you are not Blythe, then this page is not for you.</p>
+                <p>This is a page for Blythe. If you are not Blythe, then this page is not for you.</p>
+            </div>
         </div>
+        <div className="teacherCodeBody">
+            <div className="teacherCodeBoxBox">
+                <label></label>
+                <input className="teacherCodeBox" placeholder="PASSWORD" type={"text"}/>
+            </div>
+            <div className="teacherCodeCenter">
+                    <button className="teacherCodeSubmit">Submit</button>
+            </div>
         </div>
     </main>
     );
