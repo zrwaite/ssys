bea84794f6420514275f95af894ac2c2a7e625a1
Zac Waite <129032699zw@gmail.com>
2022-05-01 19:27:28 -0400

    Teacher code integrated

--- frontend/src/pages/TeacherCode/TeacherCode.jsx
+++ frontend/src/pages/TeacherCode/TeacherCode.jsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import "./TeacherCode.css";
+import {httpReq} from "../../modules/http_requests";
 
 function TeacherCode() {
-    return(
-    <main>
-        <div className="teacherCodeTop">
-            <h1>Blythe's Page</h1>
-            <div className="teacherCodeBlurb">
-                <p>This is a page for Blythe. If you are not Blythe, then this page is not for you.</p>
+    let [state, setState] = useState({
+        password: ""
+    });
+    let [code, setCode] = useState([]);
+
+    const handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        let partialState = {...state};
+        partialState[name] = value;
+        setState(partialState);
+    }
+
+    const getCodes = async () => {
+        let json = await httpReq("/api/codes/", "POST", {
+            password: state.password,
+            teacher: true
+        })
+        let response = JSON.parse(json);
+        if (response.success && response.objects) {
+            setCode(response.objects)
+        } else if (response.errors.length > 0) {
+            alert(JSON.stringify(response.errors));
+        }
+    }
+    return (
+        <main>
+            <div className="teacherCodeTop">
+                <h1>Blythe's Page</h1>
+                <div className="teacherCodeBlurb">
+                    <p>This is a page for Blythe. If you are not Blythe, then this page is not for you.</p>
+                </div>
             </div>
-        </div>
-        <div className="teacherCodeBody">
-            <div className="teacherCodeBoxBox">
-                <label></label>
-                <input className="teacherCodeBox" placeholder="PASSWORD" type={"text"}/>
-            </div>
-            <div className="teacherCodeCenter">
-                    <button className="teacherCodeSubmit">Submit</button>
-            </div>
+            <div className="teacherCodeBody">
+                <div className="teacherCodeBoxBox">
+                    <label>password</label>
+                    <input className="teacherCodeBox" placeholder="PASSWORD" type={"text"} name={"password"}
+                           onChange={handleInputChange}/>
+                </div>
+                <div className="teacherCodeCenter">
+                    <p>Code: {code}</p>
+                    <button className="teacherCodeSubmit" onClick={getCodes}>Submit</button>
+                </div>
         </div>
     </main>
     );
