60f1c488d0039c2de190b3bf3a3e4075ce175e17
Zac Waite <129032699zw@gmail.com>
2022-05-03 10:30:21 -0400

    Authorization Cors fix

--- backend/auth/index.php
+++ backend/auth/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *'); //dev allow localhost cors
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 header("Access-Control-Allow-Methods: POST, PUT, GET, DELETE");
 
 require_once __DIR__ . "/../modules/readParams.php";
--- backend/auth/signin.php
+++ backend/auth/signin.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *'); //dev allow localhost cors
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+header("Access-Control-Allow-Methods: POST, PUT, GET, DELETE");
 assert($_SERVER['REQUEST_METHOD'] == "POST");
 
 //Imports
